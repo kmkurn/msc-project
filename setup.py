a222e272e0f774bc8762df518e39727a19e1dbc1
Kemal Maulana <kemal.maulana@fastmail.com>
2017-05-27 11:40:00 +0100

    Add setup.py file to enable dev mode installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='char-rnng',
+    version='1.0.0',
+    description='RNNG with character embeddings',
+    author='Kemal Maulana',
+    author_email='kemal.maulana@fastmail.com',
+    license='MIT',
+    packages=find_packages()
+)

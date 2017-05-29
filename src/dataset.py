1b482546837bf1f8da75671e14a6ed123265706b
Kemal Maulana <kemal.maulana@fastmail.com>
2017-05-29 14:31:45 +0100

    Refactor PennTreebank class to use nltk.tree.Tree in preprocessing

--- src/dataset.py
+++ src/dataset.py
@@ -12,19 +12,21 @@ from src.utils import load_args, dump_args, augment_parser
 
 class PennTreebank:
     NULL_TAG = '-NONE-'
-    # Matches one or more spaces between two opening or closing parentheses
-    ONE_OR_MORE_SPACES_PROG = re.compile(r'(?<=\() +(?=\()|(?<=\)) +(?=\))')
-    # Matches nonterminal label with grammatical function label, e.g. NP-SBJ, PP-DIR
-    # The first group only matches the nonterminal label, e.g. NP, PP
-    NT_FUNC_LABEL_PROG = re.compile(r'(?<=\()(\w+)-[^ ]+(?= )')
 
-    def __init__(self, corpus_dir, which='train', version='3.0', corrected=True, merged=True,
+    def __init__(self, corpus_dir, which='train', version='3.0', corrected=True,
                  max_num_sentences=None):
         """An iterable class for Penn Treebank corpus.
 
         Instance of this class iterates through the corpus one line at a time. A single line
         corresponds to a single parsed sentence, even though in the original corpus, one parsed
-        sentence may span multiple lines.
+        sentence may span multiple lines. Preprocessing is also performed for each parsed
+        sentence:
+        1) Stripping off the grammatical function labels of the nonterminals
+           (e.g. NP-SBJ becomes NP)
+        2) Removing null elements (i.e. all elements with -NONE- as POS tag)
+
+        Note that this class only iterates through the combined/merged version of Penn
+        Treebank, meaning that POS tags are always included.
 
         Args:
             corpus_dir (str): Path to Penn Treebank corpus directory.
@@ -32,8 +34,6 @@ class PennTreebank:
             version (str): Version of Penn Treebank. Must be one of "2.0" or "3.0".
             corrected (bool): Whether to use the corrected version. If `False` then original
                 version will be used.
-            merged (bool): Whether to use the merged version (with POS tags). If `False` then
-                no POS tag will occur in the parsed sentence.
             max_num_sentences (int): Maximum number of sentences to load. If `None` then all
                 sentences will be loaded.
         """
@@ -47,7 +47,6 @@ class PennTreebank:
         self.which = which
         self.version = version
         self.corrected = corrected
-        self.merged = merged
         self.max_num_sentences = max_num_sentences
 
     @property
@@ -67,32 +66,28 @@ class PennTreebank:
 
     @property
     def parsed_dir(self):
-        if self.version == '2.0':
-            return 'combined' if self.merged else 'parsed'
-        if self.merged:
-            return os.path.join('parsed', 'mrg')
-        return os.path.join('parsed', 'prd')
-
-    @property
-    def ext(self):
-        return 'mrg' if self.merged else 'prd'
+        return 'combined' if self.version == '2.0' else os.path.join('parsed', 'mrg')
 
     def _get_iterator(self):
         path = os.path.join(self.corpus_dir, self.version, self.corrected_dir,
                             self.parsed_dir, 'wsj')
         for sec in self.sections:
-            glob_pattern = os.path.join(path, f'{sec:02}', f'*.{self.ext}')
+            glob_pattern = os.path.join(path, f'{sec:02}', '*.mrg')
             for filename in sorted(glob.glob(glob_pattern)):
                 with open(filename) as f:
                     lines = (line.rstrip() for line in f if line.rstrip())
                     yield from (self._preprocess_sentence(sent)
                                 for sent in self._concat_parsed_sentences(lines))
 
-    def _preprocess_sentence(self, sentence):
-        return self._remove_empty_categories(
-            self._strip_grammatical_function_label(self._squeeze_sentence(sentence)))
+    @classmethod
+    def _preprocess_sentence(cls, sentence):
+        t = Tree.fromstring(sentence, remove_empty_top_bracketing=True)
+        t = cls._remove_null_elements(t)
+        t = cls._strip_function_labels(t)
+        return cls._squeeze_line(str(t))
 
-    def _concat_parsed_sentences(self, sentences):
+    @staticmethod
+    def _concat_parsed_sentences(sentences):
         buff = []
         bracket_cnt = 0
         for sent in sentences:
@@ -108,27 +103,35 @@ class PennTreebank:
                 buff = []
                 bracket_cnt = 0
 
-    def _squeeze_sentence(self, sentence):
-        # Remove excess spaces and extra enclosing parenthesis
-        return self.ONE_OR_MORE_SPACES_PROG.sub('', sentence)[1:-1]
+    @staticmethod
+    def _squeeze_line(line):
+        return re.sub(r'  +', ' ', line.strip().replace('\n', ''))
 
-    def _strip_grammatical_function_label(self, sentence):
-        return self.NT_FUNC_LABEL_PROG.sub(r'\1', sentence)
+    @classmethod
+    def _strip_function_labels(cls, tree):
+        if cls._is_leaf(tree):
+            return tree
 
-    def _remove_empty_categories(self, sentence):
-        t = self._remove_null_elements(Tree.fromstring(sentence))
-        # Convert tree to string and squeeze it into one line
-        return re.sub(r'  +', ' ', re.sub(r'\n', '', str(t)))
+        label = tree.label()
+        ix = label.find('-')
+        new_label = label[:ix] if ix > 0 else label
+        return Tree(new_label, [cls._strip_function_labels(child) for child in tree])
 
     @classmethod
     def _remove_null_elements(cls, tree):
-        if len(tree) == 1 and isinstance(tree[0], str):
+        if len(tree) == 1 and cls._is_leaf(tree[0]):
             return None if tree.label() == cls.NULL_TAG else tree
-        new_children = [cls._remove_null_elements(child) for child in tree]
-        new_children = [x for x in new_children if x is not None]
-        if not new_children:
-            return None
-        return Tree(tree.label(), new_children)
+
+        new_children = []
+        for child in tree:
+            new_child = cls._remove_null_elements(child)
+            if new_child is not None:
+                new_children.append(new_child)
+        return Tree(tree.label(), new_children) if new_children else None
+
+    @staticmethod
+    def _is_leaf(tree):
+        return not isinstance(tree, Tree)
 
     def __iter__(self):
         return islice(self._get_iterator(), self.max_num_sentences)
@@ -207,8 +210,6 @@ if __name__ == '__main__':
                         help='corpus version (penn treebank)')
     parser.add_argument('--no-corrected', action='store_false', dest='corrected',
                         help='use non-corrected (original) version (penn treebank)')
-    parser.add_argument('--no-merged', action='store_false', dest='merged',
-                        help='use non-merged (without POS tags) version (penn treebank)')
     # IDN Treebank specific arguments
     parser.add_argument('--split-num', type=int, default=0, help='split number (idn treebank)')
     augment_parser(parser)
@@ -219,8 +220,7 @@ if __name__ == '__main__':
 
     if args.treebank == 'penn':
         treebank = PennTreebank(args.corpus_dir, which=args.which, version=args.version,
-                                corrected=args.corrected, merged=args.merged,
-                                max_num_sentences=args.max_sentences)
+                                corrected=args.corrected, max_num_sentences=args.max_sentences)
     else:
         treebank = IDNTreebank(args.corpus_dir, which=args.which, split_num=args.split_num,
                                max_num_sentences=args.max_sentences)

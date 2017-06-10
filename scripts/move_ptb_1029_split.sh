f4d4c072b2484efe4a4803cf86c341bb1b9545e6
Kemal Maulana <kemal.maulana@fastmail.com>
2017-06-10 15:06:59 +0100

    Add help and basic checking to the script

--- scripts/move_ptb_1029_split.sh
+++ scripts/move_ptb_1029_split.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+usage() {
+    echo -e "Move and rename PTB 1029 dataset to a directory.\n"
+    cat <<EOF
+Usage: $0 [-h] INPUT_DIR OUTPUT_DIR
+
+-h, --help		Display help
+
+Note that under the input directory, the dataset filenames are assumed
+to be "dataset.txt.0.train", "dataset.txt.0.valid", etc. The number
+of splits is assumed to be 5.
+EOF
+}
+
+if [ "$@" = "-h" -o "$@" = "--help" ]; then
+    usage
+    exit 0
+fi
+
+if [ "$#" -le 1 ]; then
+    usage
+    exit 1
+fi
+
 INPUT_DIR="$1"
 OUTPUT_DIR="$2"
 DATASET_FNAME="dataset.txt"

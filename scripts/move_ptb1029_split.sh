dcf8109ef6475015909527de1eb0741105191ed7
Kemal Maulana <kemal.maulana@fastmail.com>
2017-06-10 16:14:03 +0100

    Fix script and make it remove empty lines as well

--- scripts/move_ptb1029_split.sh
+++ scripts/move_ptb1029_split.sh
@@ -9,28 +9,40 @@ Usage: $0 [-h] INPUT_DIR OUTPUT_DIR
 
 Note that under the input directory, the dataset filenames are assumed
 to be "dataset.txt.0.train", "dataset.txt.0.valid", etc. The number
-of splits is assumed to be 5.
+of splits is assumed to be 5. This script also removes empty lines
+which causing problem when generating the oracle files.
 EOF
 }
 
-if [ "$@" = "-h" -o "$@" = "--help" ]; then
-    usage
-    exit 0
-fi
+while true; do
+    case "$1" in
+        -h | --help)
+            usage
+            exit 0
+            ;;
+        -*)
+            echo "Error: unknown option: $1" >&2
+            exit 1
+            ;;
+        *)
+            if [ "$#" -le 1 ]; then
+                echo "Error: expected 2 positional arguments but got $#" >&2
+                usage
+                exit 1
+            fi
+            input_dir="$1"
+            output_dir="$2"
+            break
+            ;;
+    esac
+done
 
-if [ "$#" -le 1 ]; then
-    usage
-    exit 1
-fi
-
-INPUT_DIR="$1"
-OUTPUT_DIR="$2"
 DATASET_FNAME="dataset.txt"
 WHICHES=( "train" "valid" "test" )
 NUM_SPLIT=5
 
 for i in `seq 0 $((NUM_SPLIT-1))`; do
     for which in "${WHICHES[@]}"; do
-        mv "$INPUT_DIR/$DATASET_FNAME.$i.$which" "$OUTPUT_DIR/$which.$i.txt"
+        sed '/^$/d' "$input_dir/$DATASET_FNAME.$i.$which" > "$output_dir/$which.$i.txt"
     done
 done

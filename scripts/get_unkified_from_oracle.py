80bc43d6d1690bdf06d950cdfa2877db303b3747
Kemal Maulana <kemal.maulana@fastmail.com>
2017-06-17 20:05:28 +0100

    Add script to get unkified sentence from an oracle file

---
+++ scripts/get_unkified_from_oracle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+
+from src.oracle import oracle_iter, gen_oracle_iter
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Get unkified sentence from a given oracle file.')
+    parser.add_argument('file', help='path to oracle file')
+    parser.add_argument('-g', '--gen', action='store_true',
+                        help='whether the oracle is for the generative model')
+    args = parser.parse_args()
+
+    the_iter = gen_oracle_iter if args.gen else oracle_iter
+    with open(args.file) as f:
+        for oracle in the_iter(f):
+            print(oracle.unkified)

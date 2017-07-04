7329b0cd3e8eed32fe60c33961e529440db047bb
Kemal Maulana <kemal@kkurniawan.com>
2017-07-04 15:35:37 +0100

    Change name and email

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     name='char-rnng',
     version='1.0.0',
     description='RNNG with character embeddings',
-    author='Kemal Maulana',
-    author_email='kemal.maulana@fastmail.com',
+    author='Kemal Kurniawan',
+    author_email='kemal@kkurniawan.com',
     license='MIT',
     packages=find_packages()
 )

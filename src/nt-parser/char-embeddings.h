213922bcb2c5bf659a75a2a20503948a1b6216e5
Kemal Maulana <kemal@kkurniawan.com>
2017-07-24 14:10:40 +0100

    Simplify namespace to shorten stuff

--- src/nt-parser/char-embeddings.h
+++ src/nt-parser/char-embeddings.h
@@ -7,10 +7,14 @@
 #include "cnn/expr.h"
 #include "cnn/model.h"
 
+using namespace std;
+using namespace cnn;
+using namespace cnn::expr;
+
 namespace parser {
   namespace char_embs {
     using char_t = int;
-    using word_t = std::vector<char_t>;
+    using word_t = vector<char_t>;
 
     /**
        Abstract base class for char embeddings model.
@@ -19,23 +23,21 @@ namespace parser {
     public:
       unsigned get_vocab_size() const;
       unsigned get_dim() const;
-      cnn::LookupParameters *get_char_embs() const;
-      virtual cnn::expr::Expression compute_word_embedding(cnn::ComputationGraph *cg,
-                                                           word_t word) = 0;
+      LookupParameters* get_char_embs() const;
+      virtual Expression compute_word_embedding(ComputationGraph *cg, word_t word) = 0;
     protected:
-      cnn::LookupParameters *char_embs;
+      LookupParameters *char_embs;
       unsigned vocab_size;
       unsigned dim;
 
-      BaseModel(cnn::Model *model, unsigned vocab_size, unsigned dim);
+      BaseModel(Model *model, unsigned vocab_size, unsigned dim);
       virtual ~BaseModel();
     };
 
     class AdditionModel : public BaseModel {
     public:
-      AdditionModel(cnn::Model *model, unsigned vocab_size, unsigned dim);
-      virtual cnn::expr::Expression compute_word_embedding(cnn::ComputationGraph *cg,
-                                                           word_t word) override;
+      AdditionModel(Model *model, unsigned vocab_size, unsigned dim);
+      virtual Expression compute_word_embedding(ComputationGraph *cg, word_t word) override;
     };
   }
 }

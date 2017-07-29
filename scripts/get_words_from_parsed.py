02f16aaea325f0808bfa79bf4b02a927354668a1
Kemal Maulana <kemal@kkurniawan.com>
2017-07-29 17:02:33 +0100

    Add script to get words from parsed trees

---
+++ scripts/get_words_from_parsed.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+
+from nltk.tree import Tree
+
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        print(' '.join(Tree.fromstring(line.strip()).leaves()))
